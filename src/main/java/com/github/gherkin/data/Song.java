447a304df4483b03eac9a052592547870024abd7
gherkin <niyohn@stekligan.se>
2015-10-12 23:19:18 +0200

    boilerplate

--- src/main/java/com/github/gherkin/data/Song.java
+++ src/main/java/com/github/gherkin/data/Song.java
@@ -1,4 +1,27 @@
 package com.github.gherkin.data;
 
 public class Song {
+    private Integer id;
+    private String name;
+
+    public Song(String name) {
+        id = null;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
