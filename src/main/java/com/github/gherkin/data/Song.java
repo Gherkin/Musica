907e348929b3deac465cdb6e24bc633c3a8b5e34
gherkin <niyohn@stekligan.se>
2015-10-12 23:15:45 +0200

    added the Song Datatype

---
+++ src/main/java/com/github/gherkin/data/Song.java
@@ -0,0 +1,4 @@
+package com.github.gherkin.data;
+
+public class Song {
+}
--- src/main/java/com/github/gherkin/resources/Song.java
+++ src/main/java/com/github/gherkin/resources/Song.java
@@ -1,4 +1,14 @@
 package com.github.gherkin.resources;
 
-public class Song {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Song extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
