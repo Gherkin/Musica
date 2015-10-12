dde1081516ee366d57eda6100564dfd856ff1b5b
gherkin <niyohn@stekligan.se>
2015-10-12 22:55:15 +0200

    fixed

--- src/main/sql/CreateDatabase.sql
+++ src/main/sql/CreateDatabase.sql
@@ -1,4 +1,4 @@
-DROP IF EXISTS musica;
+DROP DATABASE IF EXISTS musica;
 CREATE DATABASE musica;
 USE musica;
 create table songs(id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(512));
